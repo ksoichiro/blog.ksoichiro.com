8de6be177b5ae24134538cb3f65c7db13889989a
Soichiro Kashima <soichiro.kashima@gmail.com>
2021-02-03 01:05:22 +0900

    Migrate font setting

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,20 @@
 module.exports = {
   head: [
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1' }],
+    ['script', {}, `
+      WebFontConfig = {
+        google: { families: ['Noto Sans JP:400,700&display=swap'] },
+        active: function() {
+          sessionStorage.fonts = true;
+        }
+      };
+      (function(d) {
+        var wf = d.createElement('script'), s = d.scripts[0];
+        wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+        wf.async = true;
+        s.parentNode.insertBefore(wf, s);
+      })(document);
+    `]
   ],
   locales: {
     '/': {
