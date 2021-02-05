45fa4d4c4efc411100152c524a9fee9bd3d7594e
Soichiro Kashima <soichiro.kashima@gmail.com>
2021-02-05 23:33:50 +0900

    Enable Analytics

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,7 +20,14 @@ module.exports = {
         wf.async = true;
         s.parentNode.insertBefore(wf, s);
       })(document);
-    `]
+    `],
+    // The core Firebase JS SDK is always required and must be listed first
+    ['script', { src: '/__/firebase/8.2.6/firebase-app.js' }],
+    // Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+    ['script', { src: '/__/firebase/8.2.6/firebase-analytics.js' }],
+    // Initialize Firebase
+    ['script', { src: '/__/firebase/init.js' }],
   ],
   locales: {
     '/': {
@@ -37,6 +44,12 @@ module.exports = {
   plugins: [
     [
       '@vuepress/last-updated'
+    ],
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': '' // UA-00000000-0
+      }
     ]
   ],
   themeConfig: {
