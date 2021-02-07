461ad502e53963560e47d73f576c2c36313d57ea
Soichiro Kashima <soichiro.kashima@gmail.com>
2021-02-07 10:32:06 +0900

    Revert "Fix to get consent to use cookies"
    This reverts commit 57752fd8b32d1eac867d82a7fca80213b705751b.

--- docs/.vuepress/components/Analytics.vue
+++ docs/.vuepress/components/Analytics.vue
@@ -10,38 +10,7 @@
     <!-- Initialize Firebase -->
     <script src="/__/firebase/init.js"></script>
 
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
-
-    <script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js"></script>
-    <script>
-      window.addEventListener("load", function () {
-        window.cookieconsent.initialise({
-          palette: {
-            popup: {
-              "background": "#D8DEE9",
-              "text": "#2E3440"
-            },
-            button: {
-              "background": "#5E81AC"
-            }
-          },
-          onInitialise: function(status) {
-            if (this.hasConsented()) {
-              if (typeof firebase !== 'undefined') {
-                firebase.analytics();
-              }
-            }
-          },
-          onStatusChange: function (status) {
-            if (this.hasConsented()) {
-              if (typeof firebase !== 'undefined') {
-                firebase.analytics();
-              }
-            }
-          },
-          type: "opt-out"
-        });
-      });
-    </script>
+    <!-- Start Analytics -->
+    <script>firebase.analytics();</script>
   </div>
 </template>
