2a869cf6b8bee6b50fe3b0635d1c5166c1d94bef
Soichiro Kashima <soichiro.kashima@gmail.com>
2021-02-21 01:13:24 +0900

    Fix script to run nuxt

--- components/Footer.vue
+++ components/Footer.vue
@@ -10,7 +10,7 @@
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .footer {
   text-align: center;
 }
--- pages/ja/index.vue
+++ pages/ja/index.vue
@@ -68,7 +68,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .page {
   margin-top: 2em;
 }
