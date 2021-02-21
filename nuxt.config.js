bc4c9b50b117feda7566677c8e130039431e18af
Soichiro Kashima <soichiro.kashima@gmail.com>
2021-02-22 00:58:07 +0900

    Fix title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-content-test',
+    title: 'memorandum',
     htmlAttrs: {
       lang: 'en'
     },
