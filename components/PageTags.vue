b936d8f9877f80308f9d5eb4a3e7991218962e7e
Soichiro Kashima <soichiro.kashima@gmail.com>
2021-05-12 22:41:48 +0900

    TailwindCSS: Adjust padding for tag

--- components/PageTags.vue
+++ components/PageTags.vue
@@ -3,7 +3,7 @@
     <router-link
       v-for="tag in tags"
       :key="tag"
-      class="page-tag bg-nord2 text-nord4 py-2 px-3 mt-1 mb-2 mr-2 last:mr-0 rounded-xl transition-colors hover:no-underline hover:bg-nord2-light"
+      class="page-tag bg-nord2 text-nord4 py-1 px-2.5 mt-1 mb-2 mr-2 last:mr-0 rounded-xl transition-colors hover:no-underline hover:bg-nord2-light"
       :to="{path: localePath('/tags#' + tag)}"
     >
       {{ tag }}
